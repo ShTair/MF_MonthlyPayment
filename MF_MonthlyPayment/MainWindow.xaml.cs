086522f8666b66a58339051e6a69e7e9dd3bf0db
ShTair <a@vsh.jp>
2017-03-05 02:54:04 +0900

    リスト作成

--- MF_MonthlyPayment/MainWindow.xaml.cs
+++ MF_MonthlyPayment/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using CsvHelper;
 using MF_MonthlyPayment.Models;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Windows;
 
@@ -18,13 +19,50 @@ namespace MF_MonthlyPayment
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            var today = DateTime.Today;
+            var nm = today.AddDays(1 - today.Day).AddMonths(1);
+
             using (var writer = File.CreateText("test.txt"))
             using (var csv = new CsvWriter(writer))
             {
                 csv.Configuration.RegisterClassMap<PaymentItem.Map>();
 
                 csv.WriteHeader<PaymentItem>();
-                csv.WriteRecord(new PaymentItem { Amount = 1000, Date = DateTime.Now });
+                csv.WriteRecords(CreateItems(nm, "test", 1000, 24, "a", "b"));
+            }
+        }
+
+        private IEnumerable<PaymentItem> CreateItems(DateTime start, string content, int amount, int count, string kind1, string kind2)
+        {
+            var limit = DateTime.Today.AddYears(1);
+            var date = start;
+
+            for (int i = 0; i < count; i++)
+            {
+                var v = amount / (count - i);
+
+                yield return new PaymentItem
+                {
+                    Date = date,
+                    Content = content,
+                    Amount = -v,
+                    Bank = "前払いプール",
+                    Kind1 = kind1,
+                    Kind2 = kind2,
+                };
+
+                yield return new PaymentItem
+                {
+                    Date = date,
+                    Content = content,
+                    Amount = v,
+                    Bank = "前払い元",
+                    Kind1 = "収入",
+                    Kind2 = "その他入金",
+                };
+
+                amount -= v;
+                date = start.AddMonths(i + 1);
             }
         }
     }
